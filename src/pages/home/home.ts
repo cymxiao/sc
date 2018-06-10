7665a8e5fd5d020f172c6f6ad5fe1be503579233
caiyaming <caiyaming@me.com>
2018-06-10 19:43:09 +0800

    add media plugin

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { timer } from 'rxjs/observable/timer';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,18 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
 
+  timeInterval : number;
+  showAlert:boolean;
+
+  constructor(public navCtrl: NavController) {
+    
+  }
+
+  save(){
+    //timer(this.timeInterval*60*1000).subscribe(() => this.showAlert = false);  
+    timer(this.timeInterval*60*1000).subscribe(() => {
+      this.showAlert = false}); 
   }
 
 }
