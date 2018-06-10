65f0a996ceba7fc09906510a954e44dd0bf9da62
caiyaming <caiyaming@me.com>
2018-06-10 22:01:44 +0800

    #feature# set time interval works, such each 5 seconds, exec a function.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -21,12 +21,18 @@ export class HomePage {
 
   save() {
     //timer(this.timeInterval*60*1000).subscribe(() => 
-    //do {
-      timer(this.timeInterval * 1000).subscribe(() => {
-        this.showAlert = false;
-        this.playSound();
-      });
-    //} while (true);
+    console.log(this.timeInterval);
+    /*
+      timer takes a second argument, how often to emit subsequent values
+      in this case we will emit first value after param1 second and subsequent
+      values every param2 seconds after
+    */
+    timer(this.timeInterval * 1000,this.timeInterval * 1000).subscribe(() => {
+      this.showAlert = false;
+      this.playSound();
+      //this.save();
+    });
+    
   }
 
   playSound() {
@@ -39,12 +45,20 @@ export class HomePage {
     if (file) {
       //file.onStatusUpdate.subscribe(status => console.log(status)); // fires when file status changes
 
-      file.onSuccess.subscribe(() => console.log('Action is successful'));
+      file.onSuccess.subscribe(() => {console.log('Action is successful');
+        //this.save();
+      });
 
       file.onError.subscribe(error => console.log('Error!', error));
 
       // // play the file
       file.play();
+
+      // release the native audio resource
+      // Platform Quirks:
+      // iOS simply create a new instance and the old one will be overwritten
+      // Android you must call release() to destroy instances of media when you are done
+      //file.release();
     }
 
   }
